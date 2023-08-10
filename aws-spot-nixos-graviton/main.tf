396bd3c3e4a8b9b01133a52e12cadbca12151c62
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2023-08-09 21:43:10 -0700

    modularised remaining templates

--- aws-spot-nixos-graviton/main.tf
+++ aws-spot-nixos-graviton/main.tf
@@ -67,160 +67,43 @@ variable "home_disk_size" {
   type = number
   default = 10
 }
-
-locals {
-  # rg -F aarch64-linux.hvm-ebs' nixos/modules/virtualisation/amazon-ec2-amis.nix \
-  #   | grep 22.05 \
-  #   | sed 's/ = /./' \
-  #   | cut -d. -f3,6 \
-  #   | sed 's|\(.*\)\."\(.*\)";$|\1 = "\2",|' \
-  #   | sort
-  images = {
-    af-south-1 = "ami-0a9b83913abd61694",
-    ap-east-1 = "ami-03966ad4547f532b7",
-    ap-northeast-1 = "ami-0eb7e152c8d5aae7d",
-    ap-northeast-2 = "ami-08369e00c5528762b",
-    ap-northeast-3 = "ami-0fa14b8d48cdd57c3",
-    ap-south-1 = "ami-0f2ca3b542ff0913b",
-    ap-southeast-1 = "ami-087def0511ef2687d",
-    ap-southeast-2 = "ami-0aa90985199011f04",
-    ap-southeast-3 = "ami-0c86c52790deefa23",
-    ca-central-1 = "ami-06e932cc9c20403e4",
-    eu-central-1 = "ami-07680df1026a9b54c",
-    eu-north-1 = "ami-0cbe9f2725e4de706",
-    eu-south-1 = "ami-01a83c3892925765f",
-    eu-west-1 = "ami-08114069426233360",
-    eu-west-2 = "ami-049024d086d039b54",
-    eu-west-3 = "ami-0c0ebe20ebfc635a1",
-    me-south-1 = "ami-0d662fcaac553e945",
-    sa-east-1 = "ami-0888c8f703e00fdb8",
-    us-east-1 = "ami-03536a13324333073",
-    us-east-2 = "ami-067611519fa817aaa",
-    us-west-1 = "ami-0f96be48071c13ab2",
-    us-west-2 = "ami-084bc5d777585adfb",
-  }
-
-  # User data is used to stop/start AWS instances. See:
-  # https://github.com/hashicorp/terraform-provider-aws/issues/22
-
-  user_data_start = <<EOT
-#!/usr/bin/env bash
-exec > $HOME/provision.log
-exec 2> $HOME/provision.err
-
-# this doesn't seem to be sourced beforehand
-source /etc/profile
-
-nixos-rebuild switch --flake '${var.flake_uri}'
-hash -r
-export CODER_AGENT_TOKEN=${coder_agent.box.token}
-sudo --preserve-env=CODER_AGENT_TOKEN -u denbeigh bash -c '${coder_agent.box.init_script}'
-EOT
-
-  user_data_end = <<EOT
-#!/usr/bin/env bash
-sudo shutdown -h now
-EOT
-}
-
-provider "aws" {
-  region = var.region
-}
-
 data "coder_workspace" "me" {
 }
 
-resource "coder_agent" "box" {
-  arch = "arm64"
-  auth = "aws-instance-identity"
-  os   = "linux"
-}
-
-resource "coder_agent_instance" "box" {
+module "box" {
   count = data.coder_workspace.me.start_count
-  agent_id = coder_agent.box.id
-  instance_id = aws_spot_instance_request.box[0].spot_instance_id
+  source         = "./modules/instance"
+  arch           = "aarch64"
+  is_spot        = true
+  region         = var.region
+  flake_uri      = var.flake_uri
+  instance_type  = var.instance_type
+  root_disk_size = var.root_disk_size
 }
 
-resource "aws_ebs_volume" "home_disk" {
-  availability_zone = "${var.region}a"
-  size = var.home_disk_size
-  type = "gp3"
+module "common" {
+  source         = "./modules/common"
 
-  tags = {
-    App = "coder"
-    CoderPurpose = "home-disk-volume"
-    CoderUser = data.coder_workspace.me.owner
-  }
-}
-
-resource "aws_volume_attachment" "box_home_disk" {
-  count = data.coder_workspace.me.start_count
-  # NOTE: This is tied to a volume mount in NixOS configs for Coder workspaces.
-  device_name = "/dev/xvdb"
-  volume_id = aws_ebs_volume.home_disk.id
-  instance_id = aws_spot_instance_request.box[0].spot_instance_id
-}
-
-resource "aws_spot_instance_request" "box" {
-  count = data.coder_workspace.me.start_count
-  ami               = local.images[var.region]
-  availability_zone = "${var.region}a"
-  instance_type     = var.instance_type
-
-  root_block_device {
-    volume_size = var.root_disk_size
-    volume_type = "gp3"
-  }
-
-  wait_for_fulfillment = true
-  instance_interruption_behavior = "stop"
-
-  user_data = local.user_data_start
-  tags = {
-    Name = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}"
-    App = "coder"
-    CoderPurpose = "workspace-spot-instance"
-    CoderUser = data.coder_workspace.me.owner
-  }
-}
-
-data "aws_ec2_spot_price" "box" {
-  instance_type     = "${var.instance_type}"
-  availability_zone = "${var.region}a"
-
-  filter {
-    name   = "product-description"
-    values = ["Linux/UNIX"]
-  }
+  region         = var.region
+  instance_id    = data.coder_workspace.me.start_count == 0 ? null : module.box[0].instance_id
+  home_disk_size = var.home_disk_size
 }
 
 resource "coder_metadata" "workspace_info" {
-  count = data.coder_workspace.me.start_count
-  resource_id = aws_spot_instance_request.box[0].id
-  item {
-    key = "cost"
-    value = "${data.aws_ec2_spot_price.box.spot_price}/hr"
-  }
+  count       = data.coder_workspace.me.start_count
+  resource_id = module.box[0].instance_id
   item {
     key   = "region"
     value = var.region
   }
   item {
     key   = "instance type"
-    value = aws_spot_instance_request.box[0].instance_type
+    // TODO: Expose this through instance?
+    value = var.instance_type
   }
-  item {
-    key   = "disk size (root)"
-    value = "${aws_spot_instance_request.box[0].root_block_device[0].volume_size} GiB"
-  }
-}
-
-resource "coder_metadata" "home_disk_info" {
-  resource_id = aws_ebs_volume.home_disk.id
-
-  item {
-    key   = "disk size (home)"
-    value = "${aws_ebs_volume.home_disk.size} GiB"
-  }
+  // TODO: Expose this again?
+  // item {
+  //   key   = "root disk"
+  //   value = "${aws_spot_instance.box[0].root_block_device[0].volume_size} GiB"
+  // }
 }
--- aws-spot-nixos/main.tf
+++ aws-spot-nixos/main.tf
@@ -69,160 +69,43 @@ variable "home_disk_size" {
   type = number
   default = 10
 }
-
-locals {
-  # rg -F 'x86_64-linux.hvm-ebs' nixos/modules/virtualisation/amazon-ec2-amis.nix \
-  #   | grep 22.05 \
-  #   | sed 's/ = /./' \
-  #   | cut -d. -f3,6 \
-  #   | sed 's|\(.*\)\."\(.*\)";$|\1 = "\2",|' \
-  #   | sort
-  images = {
-    af-south-1 = "ami-0d3a6166c1ea4d7b4",
-    ap-east-1 = "ami-06445325c360470d8",
-    ap-northeast-1 = "ami-009c422293bcf3721",
-    ap-northeast-2 = "ami-0bfc0397525a67ed8",
-    ap-northeast-3 = "ami-0a1fb4d4e08a6065e",
-    ap-south-1 = "ami-07ad258dcc69239d2",
-    ap-southeast-1 = "ami-0f59f7f33cba8b1a4",
-    ap-southeast-2 = "ami-0d1e49fe30aec165d",
-    ap-southeast-3 = "ami-0f5cb24a1e3fc62dd",
-    ca-central-1 = "ami-0551a595ba7916462",
-    eu-central-1 = "ami-0702eee2e75d541d1",
-    eu-north-1 = "ami-0fc6838942cb7d9cb",
-    eu-south-1 = "ami-0df9463b8965cdb80",
-    eu-west-1 = "ami-00badba5cfa0a0c0d",
-    eu-west-2 = "ami-08f3c1eb533a42ac1",
-    eu-west-3 = "ami-04b50c79dc4009c97",
-    me-south-1 = "ami-05c52087afab7024d",
-    sa-east-1 = "ami-0732aa0f0c28f281b",
-    us-east-1 = "ami-0223db08811f6fb2d",
-    us-east-2 = "ami-0a743534fa3e51b41",
-    us-west-1 = "ami-0d72ab697beab5ea5",
-    us-west-2 = "ami-034946f0c47088751",
-  }
-
-  # User data is used to stop/start AWS instances. See:
-  # https://github.com/hashicorp/terraform-provider-aws/issues/22
-
-  user_data_start = <<EOT
-#!/usr/bin/env bash
-exec > $HOME/provision.log
-exec 2> $HOME/provision.err
-
-# this doesn't seem to be sourced beforehand
-source /etc/profile
-
-nixos-rebuild switch --flake '${var.flake_uri}'
-hash -r
-export CODER_AGENT_TOKEN=${coder_agent.box.token}
-sudo --preserve-env=CODER_AGENT_TOKEN -u denbeigh bash -c '${coder_agent.box.init_script}'
-EOT
-
-  user_data_end = <<EOT
-#!/usr/bin/env bash
-sudo shutdown -h now
-EOT
-}
-
-provider "aws" {
-  region = var.region
-}
-
 data "coder_workspace" "me" {
 }
 
-resource "coder_agent" "box" {
-  arch = "amd64"
-  auth = "aws-instance-identity"
-  os   = "linux"
-}
-
-resource "coder_agent_instance" "box" {
+module "box" {
   count = data.coder_workspace.me.start_count
-  agent_id = coder_agent.box.id
-  instance_id = aws_spot_instance_request.box[0].spot_instance_id
+  source         = "./modules/instance"
+  arch           = "x86_64"
+  is_spot        = true
+  region         = var.region
+  flake_uri      = var.flake_uri
+  instance_type  = var.instance_type
+  root_disk_size = var.root_disk_size
 }
 
-resource "aws_ebs_volume" "home_disk" {
-  availability_zone = "${var.region}a"
-  size = var.home_disk_size
-  type = "gp3"
+module "common" {
+  source         = "./modules/common"
 
-  tags = {
-    App = "coder"
-    CoderPurpose = "home-disk-volume"
-    CoderUser = data.coder_workspace.me.owner
-  }
-}
-
-resource "aws_volume_attachment" "box_home_disk" {
-  count = data.coder_workspace.me.start_count
-  # NOTE: This is tied to a volume mount in NixOS configs for Coder workspaces.
-  device_name = "/dev/xvdb"
-  volume_id = aws_ebs_volume.home_disk.id
-  instance_id = aws_spot_instance_request.box[0].spot_instance_id
-}
-
-resource "aws_spot_instance_request" "box" {
-  count = data.coder_workspace.me.start_count
-  ami               = local.images[var.region]
-  availability_zone = "${var.region}a"
-  instance_type     = var.instance_type
-
-  wait_for_fulfillment = true
-  instance_interruption_behavior = "stop"
-
-  root_block_device {
-    volume_size = var.root_disk_size
-    volume_type = "gp3"
-  }
-
-  user_data = data.coder_workspace.me.transition == "start" ? local.user_data_start : local.user_data_end
-  tags = {
-    Name = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}"
-    App = "coder"
-    CoderPurpose = "workspace-spot-instance"
-    CoderUser = data.coder_workspace.me.owner
-  }
-}
-
-data "aws_ec2_spot_price" "box" {
-  instance_type     = "${var.instance_type}"
-  availability_zone = "${var.region}a"
-
-  filter {
-    name   = "product-description"
-    values = ["Linux/UNIX"]
-  }
+  region         = var.region
+  instance_id    = data.coder_workspace.me.start_count == 0 ? null : module.box[0].instance_id
+  home_disk_size = var.home_disk_size
 }
 
 resource "coder_metadata" "workspace_info" {
-  count = data.coder_workspace.me.start_count
-  resource_id = aws_spot_instance_request.box[0].id
-  item {
-    key = "cost"
-    value = "${data.aws_ec2_spot_price.box.spot_price}/hr"
-  }
+  count       = data.coder_workspace.me.start_count
+  resource_id = module.box[0].instance_id
   item {
     key   = "region"
     value = var.region
   }
   item {
     key   = "instance type"
-    value = aws_spot_instance_request.box[0].instance_type
+    // TODO: Expose this through instance?
+    value = var.instance_type
   }
-  item {
-    key   = "disk size (root)"
-    value = "${aws_spot_instance_request.box[0].root_block_device[0].volume_size} GiB"
-  }
-}
-
-resource "coder_metadata" "home_disk_info" {
-  resource_id = aws_ebs_volume.home_disk.id
-
-  item {
-    key   = "disk size (home)"
-    value = "${aws_ebs_volume.home_disk.size} GiB"
-  }
+  // TODO: Expose this again?
+  // item {
+  //   key   = "root disk"
+  //   value = "${aws_spot_instance.box[0].root_block_device[0].volume_size} GiB"
+  // }
 }
