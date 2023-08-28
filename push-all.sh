14073d9cfd020d7a8aaf6f86602bd30ce8704cb5
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2023-08-28 16:37:38 -0500

    misc: made pushing script more robust

--- push-all.sh
+++ push-all.sh
@@ -3,10 +3,11 @@
 set -euo pipefail
 
 VERB="${1:-push}"
+REPO_ROOT="$(git rev-parse --show-toplevel)"
 
-find values -name "*.yaml" -or -name "*.yml" | while read TEMPLATE_FILE
+find "$REPO_ROOT/values" -name "*.yaml" -or -name "*.yml" | while read TEMPLATE_FILE
 do
     TEMPLATE_NAME="$(basename $TEMPLATE_FILE | sed -E 's/\.ya?ml$//')"
 
-    coder templates "$VERB" --yes -d . "$TEMPLATE_NAME" --variables-file "$TEMPLATE_FILE"
+    coder templates "$VERB" --yes -d "$REPO_ROOT" --variables-file "$TEMPLATE_FILE" "$TEMPLATE_NAME"
 done
